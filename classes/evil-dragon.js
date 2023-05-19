54898d457936596d9867780f0ea77726702ea345
Dom <dtaplin21@aol.com>
2023-05-18 21:41:40 -0700

    evil

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,34 @@
-// Your code here
+const dragon = require("./dragon.js")
+
+class EvilDragon extends dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+    dontInviteThemOverForDinner() {
+     this.evilDoings.map((evilDoing) => {
+        console.log(`${this.name} will ${evilDoing}`)
+     })
+    }
+
+     burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+     }
+    }
+  // Your code here
+module.exports = EvilDragon
+
+const enderDragon = new EvilDragon(
+    "Ender Dragon",
+    "black", [
+      "spit a fireball at Steve",
+      "rule over all the Endermen",
+      "destroy all blocks in its way"
+    ],
+    "Steve"
+  );
+  console.log(enderDragon);
+  enderDragon.dontInviteThemOverForDinner();
+  console.log(enderDragon.breathesFire());
+  console.log(enderDragon.burnsNemesis());
