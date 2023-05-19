84777ce06c102cf793893fbab03def1a9fa7496c
Nikola Milinovich <nmilinovich1@gmail.com>
2023-05-18 21:09:23 -0700

    phase 1 and 2 done

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,20 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+  this.name = name;
+  this.color = color
 
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...Dragon) {
+    return Dragon.map((Dragon) => Dragon.name)
+  }
+}
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
